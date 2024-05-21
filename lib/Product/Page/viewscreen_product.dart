b6346f456aeb73aa2d828f0169c57021fce06a36
Yazed Elkholy <yazedelkholy2003@gmail.com>
2024-05-21 19:40:51 +0300

    make view page scrollable

--- lib/Product/Page/viewscreen_product.dart
+++ lib/Product/Page/viewscreen_product.dart
@@ -1,17 +1,18 @@
 import 'dart:typed_data';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:path/path.dart';
 import '../Database/entity_model/product_model.dart';
 import '../cubit/product_cubit.dart';
 
 class ViewscreenProduct extends StatelessWidget {
   const ViewscreenProduct({super.key});
   static const routeName = '/viewscreen';
+
   @override
   Widget build(BuildContext context) {
     final product = ModalRoute.of(context)?.settings.arguments as ProductModel;
     final productCubit = context.read<ProductCubit>();
+    final mediaQuery = MediaQuery.of(context);
 
     return BlocProvider.value(
       value: productCubit,
@@ -29,8 +30,13 @@ class ViewscreenProduct extends StatelessWidget {
           ),
           toolbarHeight: 50,
         ),
-        body: Padding(
-          padding: const EdgeInsets.all(16.0),
+        body: SingleChildScrollView(
+          padding: EdgeInsets.only(
+            left: 16.0,
+            right: 16.0,
+            top: 16.0,
+            bottom: mediaQuery.viewInsets.bottom + 16.0, // Adjusts for bottom overflow
+          ),
           child: Column(
             mainAxisAlignment: MainAxisAlignment.start,
             children: [
@@ -41,14 +47,14 @@ class ViewscreenProduct extends StatelessWidget {
                   height: 300,
                 ),
               ),
-                const SizedBox(height: 10.0),
+              const SizedBox(height: 10.0),
               Row(
                 mainAxisAlignment: MainAxisAlignment.spaceAround,
                 children: [
                   Text(
                     product.name ?? 'Product Name',
                     style: TextStyle(
-                        fontSize: 20.0,
+                      fontSize: 20.0,
                       fontWeight: FontWeight.bold,
                       color: Theme.of(context).colorScheme.inversePrimary,
                     ),
@@ -56,7 +62,7 @@ class ViewscreenProduct extends StatelessWidget {
                   Text(
                     '${product.price} EGP',
                     style: const TextStyle(
-                        fontSize: 18.0,
+                      fontSize: 18.0,
                       fontWeight: FontWeight.bold,
                       color: Color(0xFFFF8F00),
                     ),
@@ -64,46 +70,46 @@ class ViewscreenProduct extends StatelessWidget {
                 ],
               ),
               const SizedBox(height: 16.0),
-                Column(
-                  children: [
-                    const Row(
-                      mainAxisAlignment: MainAxisAlignment.start,
-                      children: [
-                        Text(
-                          'Description',
-                          style: TextStyle(
-                            fontWeight: FontWeight.w900,
-                            fontSize: 19,
-                          ),
+              Column(
+                children: [
+                  const Row(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    children: [
+                      Text(
+                        'Description',
+                        style: TextStyle(
+                          fontWeight: FontWeight.w900,
+                          fontSize: 19,
                         ),
-                      ],
+                      ),
+                    ],
+                  ),
+                  Text(
+                    product.description ?? 'Product Description',
+                    style: const TextStyle(
+                      fontSize: 15.0,
+                      fontWeight: FontWeight.w400,
                     ),
-              Text(
-                product.description ?? 'Product Description',
-                style: const TextStyle(
-                          fontSize: 15.0, fontWeight: FontWeight.w400
-                ),
+                  ),
+                ],
               ),
-                  ],
-                ),
-                const SizedBox(height: 80.0),
+              const SizedBox(height: 80.0),
               Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                 children: [
                   IconButton(
-                      icon: const Icon(
-                        Icons.remove,
-                        color: Colors.white,
-                        size: 20,
-                      ),
-                      style: IconButton.styleFrom(
-                        backgroundColor: Colors.amber.shade800,
-                        shape: const CircleBorder(),
-                      ),
+                    icon: const Icon(
+                      Icons.remove,
+                      color: Colors.white,
+                      size: 20,
+                    ),
+                    style: IconButton.styleFrom(
+                      backgroundColor: Colors.amber.shade800,
+                      shape: const CircleBorder(),
+                    ),
                     onPressed: () {
                       productCubit.decreaseProductQuantity(product);
                     },
-                      
                   ),
                   Text(
                     '${product.quantity}',
@@ -113,15 +119,15 @@ class ViewscreenProduct extends StatelessWidget {
                     ),
                   ),
                   IconButton(
-                      icon: const Icon(
-                        Icons.add,
-                        color: Colors.white,
-                        size: 20,
-                      ),
-                      style: IconButton.styleFrom(
-                        backgroundColor: Colors.amber.shade800,
-                        shape: const CircleBorder(),
-                      ),
+                    icon: const Icon(
+                      Icons.add,
+                      color: Colors.white,
+                      size: 20,
+                    ),
+                    style: IconButton.styleFrom(
+                      backgroundColor: Colors.amber.shade800,
+                      shape: const CircleBorder(),
+                    ),
                     onPressed: () {
                       productCubit.increaseProductQuantity(product);
                     },
@@ -129,29 +135,28 @@ class ViewscreenProduct extends StatelessWidget {
                 ],
               ),
               const SizedBox(height: 16.0),
-             
-              ],
-            ),
+            ],
           ),
-          bottomNavigationBar: Container(
-            decoration: BoxDecoration(
-              borderRadius: const BorderRadius.only(
-                topLeft: Radius.circular(16.0),
-                topRight: Radius.circular(16.0),
-              ),
-              color: Theme.of(context).colorScheme.surface,
+        ),
+        bottomNavigationBar: Container(
+          decoration: BoxDecoration(
+            borderRadius: const BorderRadius.only(
+              topLeft: Radius.circular(16.0),
+              topRight: Radius.circular(16.0),
             ),
-            child: Padding(
-              padding: const EdgeInsets.all(16.0),
-              child: ElevatedButton(
-                onPressed: () {
-                  productCubit.addItemToCart(
-                    product.id ?? 0,
-                    true,
-                    product.quantity as int,
-                  );
-                  ScaffoldMessenger.of(context).showSnackBar(
-                    const SnackBar(
+            color: Theme.of(context).colorScheme.surface,
+          ),
+          child: Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: ElevatedButton(
+              onPressed: () {
+                productCubit.addItemToCart(
+                  product.id ?? 0,
+                  true,
+                  product.quantity as int,
+                );
+                ScaffoldMessenger.of(context).showSnackBar(
+                  const SnackBar(
                       content: Text('Added to cart'),
                       backgroundColor: Colors.amber,
                       duration: Duration(seconds: 1),
@@ -169,28 +174,27 @@ class ViewscreenProduct extends StatelessWidget {
                         ),
                       ),
                     ),
-                  );
-                },
-                style: ElevatedButton.styleFrom(
-                  backgroundColor: const Color(0xFFFF8F00),
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(10.0),
-                  ),
-                  minimumSize: const Size(double.infinity, 50),
+                );
+              },
+              style: ElevatedButton.styleFrom(
+                backgroundColor: const Color(0xFFFF8F00),
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(10.0),
                 ),
-                child: const Text(
-                  'Add to cart',
-                  style: TextStyle(
-                    color: Colors.white,
-                    fontWeight: FontWeight.bold,
-                    fontSize: 18.0,
-                  ),
+                minimumSize: const Size(double.infinity, 50),
+              ),
+              child: const Text(
+                'Add to cart',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontWeight: FontWeight.bold,
+                  fontSize: 18.0,
                 ),
               ),
             ),
           ),
-)
-      );
-    
+        ),
+      ),
+    );
   }
 }
